9b8b99105801ae834d5227d68eb821ab8ac00b23
Ravikanth Marri <ravikanth.marri@gmail.com>
2023-12-23 00:07:41 +0000

    Initial Commit

---
+++ PeopleSwiftUIMVVM/PeopleSwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleSwiftUIMVVMApp.swift
+//  PeopleSwiftUIMVVM
+//
+//  Created by Ravikanth on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleSwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
